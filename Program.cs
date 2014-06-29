993e6fd839b1a5a87ae26799612b60645fffbbf9
James Waples <jamwaffles@gmail.com>
2014-06-29 22:02:32 +0100

    Open a wavefront file and print it to the console.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace ObjLoader
 	{
 		public static void Main (string[] args)
 		{
+			WavefrontModel model = new WavefrontModel ("./sphere-lowpoly.obj");
 
+			Console.ReadKey ();
 		}
 	}
 }
--- WavefrontModel.cs
+++ WavefrontModel.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics;
 
 namespace ObjLoader
 {
+	struct Face 
+	{
+		int v1, v2, v3;
+		int vn1, vn2, vn3;
+	}
+
 	public class WavefrontModel
 	{
-		public WavefrontModel ()
+		List<Vector3d> vertices;
+		List<Vector3d> normals;
+		List<Face> faces;
+
+		public WavefrontModel (string filename)
 		{
+			load (filename);
+		}
+
+		// Load and parse the .OBJ file
+		private int load(string filename) 
+		{
+			// Load entire file into an array (maybe bad for memory?)
+			string[] lines = System.IO.File.ReadAllLines(filename);
+
+			foreach (string line in lines)
+			{
+				Console.WriteLine(line);
+			}
+
+			return 0;
+		}
+
+		public void draw() 
+		{
+
 		}
 	}
 }
