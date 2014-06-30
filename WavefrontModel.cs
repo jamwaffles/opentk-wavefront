c3d65842d1415938cd293d2d4dd69edb074996d1
James Waples <jamwaffles@gmail.com>
2014-06-30 07:52:41 +0100

    Parse and store in memory vertices, normals and faces.

--- WavefrontModel.cs
+++ WavefrontModel.cs
@@ -7,15 +7,15 @@ namespace ObjLoader
 {
 	struct Face 
 	{
-		int v1, v2, v3;
-		int vn1, vn2, vn3;
+		public int v1, v2, v3;
+		public int vn1, vn2, vn3;
 	}
 
 	public class WavefrontModel
 	{
-		List<Vector3d> vertices;
-		List<Vector3d> normals;
-		List<Face> faces;
+		List<Vector3d> vertices = new List<Vector3d>();
+		List<Vector3d> normals = new List<Vector3d>();
+		List<Face> faces = new List<Face>();
 
 		public WavefrontModel (string filename)
 		{
@@ -30,8 +30,49 @@ namespace ObjLoader
 
 			foreach (string line in lines)
 			{
-				Console.WriteLine(line);
+				string[] tokens = line.Split (' ');
+
+				switch (tokens [0]) {
+				case "v":
+					vertices.Add (new Vector3d() {
+						X = Convert.ToDouble (tokens [1]),
+						Y = Convert.ToDouble (tokens [2]),
+						Z = Convert.ToDouble (tokens [3])
+					});
+					break;
+				case "vn":
+					normals.Add (new Vector3d () {
+						X = Convert.ToDouble (tokens [1]),
+						Y = Convert.ToDouble (tokens [2]),
+						Z = Convert.ToDouble (tokens [3])
+					});
+					break;
+				case "f":
+					string[] point0 = tokens [1].Split ('/');
+					string[] point1 = tokens [2].Split ('/');
+					string[] point2 = tokens [3].Split ('/');
+
+					faces.Add (new Face () {
+						v1 = Convert.ToInt32(point0[0]),
+						vn1 = Convert.ToInt32(point0[2]),
+
+						v2 = Convert.ToInt32(point1[0]),
+						vn2 = Convert.ToInt32(point1[2]),
+
+						v3 = Convert.ToInt32(point2[0]),
+						vn3 = Convert.ToInt32(point2[2])
+					});
+
+					break;
+				default:
+					Console.WriteLine ("Parse info: " + line);
+					break;
+				}
 			}
+
+			Console.WriteLine ("Number of vertices: " + vertices.Count);
+			Console.WriteLine ("Number of normals: " + normals.Count);
+			Console.WriteLine ("Number of faces: " + faces.Count);
 
 			return 0;
 		}
