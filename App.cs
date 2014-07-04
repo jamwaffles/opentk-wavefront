8270a5a08976afc9682230e17b63d3be7f41721b
James Waples <jamwaffles@gmail.com>
2014-07-04 21:57:03 +0100

    Boilerplate to open a double buffered window and paint it blue.

--- App.cs
+++ App.cs
@@ -1,9 +1,62 @@
 ﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
 namespace ObjLoader {
-	public class ObjLoader {
+	public class ObjLoaderApp: GameWindow
+	{
+		public ObjLoaderApp(): base(
+			800, 
+			600, 
+			new OpenTK.Graphics.GraphicsMode(32, 16, 0, 4), 
+			"Obj Loader", 
+			GameWindowFlags.Default, 
+			DisplayDevice.Default, 
+			3,		// Major version
+			3,		// Minor version
+			OpenTK.Graphics.GraphicsContextFlags.Default | OpenTK.Graphics.GraphicsContextFlags.Debug)
+		{
+			Keyboard.KeyDown += HandleKeyDown;
+		}
+
+		void HandleKeyDown (object sender, KeyboardKeyEventArgs e)
+		{
+			if (e.Key == Key.Escape) Exit ();
+		}
+
+		protected override void OnLoad (EventArgs e)
+		{
+			base.OnLoad (e);
+
+			GL.ClearColor (Color.CornflowerBlue);
+		}
+
+		protected override void OnRenderFrame (FrameEventArgs e)
+		{
+			base.OnRenderFrame (e);
+
+			GL.Clear (ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+
+			SwapBuffers ();
+		}
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+		}
+	}
+
+	public class App {
+		[STAThread]
 		public static void Main() {
-			Console.WriteLine ("Hello world");
+			using (ObjLoaderApp app = new ObjLoaderApp ()) 
+			{
+				app.Run (60.0);
+			}
 		}
 	}
 }
