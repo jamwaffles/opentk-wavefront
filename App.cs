6ad286ca3891c049d2b9b0742063272fe4870bea
James Waples <jamwaffles@gmail.com>
2014-07-05 10:29:15 +0100

    Spinning red cube.

--- App.cs
+++ App.cs
@@ -22,40 +22,96 @@ void main()
 
 		string fragmentShaderSource = @"
 #version 330
- 
-out vec4 FragColor;
+
+out vec4 out_frag_color;
  
 void main()
 {
-	FragColor = vec4(0.5, 0.8, 1.0, 1.0);
+	out_frag_color = vec4(1.0, 0.0, 0.0, 1.0);
 }";
 
 		Vector3[] positionData = new Vector3[]{
-			new Vector3(-1.0f, -1.0f,  1.0f),
-			new Vector3( 1.0f, -1.0f,  1.0f),
-			new Vector3( 1.0f,  1.0f,  1.0f),
-			new Vector3(-1.0f,  1.0f,  1.0f),
-			new Vector3(-1.0f, -1.0f, -1.0f),
-			new Vector3( 1.0f, -1.0f, -1.0f), 
-			new Vector3( 1.0f,  1.0f, -1.0f),
-			new Vector3(-1.0f,  1.0f, -1.0f) };
+			// Front face
+			new Vector3 (-1.0f, -1.0f, 1.0f), 
+			new Vector3 (1.0f, -1.0f, 1.0f), 
+			new Vector3 (1.0f, 1.0f, 1.0f), 
+			new Vector3 (-1.0f, 1.0f, 1.0f),
+			// Right face
+			new Vector3 (1.0f, -1.0f, 1.0f), 
+			new Vector3 (1.0f, -1.0f, -1.0f), 
+			new Vector3 (1.0f, 1.0f, -1.0f), 
+			new Vector3 (1.0f, 1.0f, 1.0f),
+			// Back face
+			new Vector3 (1.0f, -1.0f, -1.0f), 
+			new Vector3 (-1.0f, -1.0f, -1.0f), 
+			new Vector3 (-1.0f, 1.0f, -1.0f), 
+			new Vector3 (1.0f, 1.0f, -1.0f),
+			// Left face
+			new Vector3 (-1.0f, -1.0f, -1.0f), 
+			new Vector3 (-1.0f, -1.0f, 1.0f), 
+			new Vector3 (-1.0f, 1.0f, 1.0f), 
+			new Vector3 (-1.0f, 1.0f, -1.0f),
+			// Top Face	
+			new Vector3 (-1.0f, 1.0f, 1.0f), 
+			new Vector3 (1.0f, 1.0f, 1.0f),
+			new Vector3 (1.0f, 1.0f, -1.0f), 
+			new Vector3 (-1.0f, 1.0f, -1.0f),
+			// Bottom Face
+			new Vector3 (1.0f, -1.0f, 1.0f), 
+			new Vector3 (-1.0f, -1.0f, 1.0f),
+			new Vector3 (-1.0f, -1.0f, -1.0f), 
+			new Vector3 (1.0f, -1.0f, -1.0f) };
 
-		int[] indexData = new int[]{
-			// front face
-			0, 1, 2, 2, 3, 0,
-			// top face
-			3, 2, 6, 6, 7, 3,
-			// back face
-			7, 6, 5, 5, 4, 7,
-			// left face
-			4, 0, 3, 3, 7, 4,
-			// bottom face
-			0, 1, 5, 5, 4, 0,
-			// right face
-			1, 5, 6, 6, 2, 1, };
+		Vector3[] normalData = new Vector3[] {
+			// Front face
+			new Vector3 ( 0f, 0f, 1f), 
+			new Vector3 ( 0f, 0f, 1f),
+			new Vector3 ( 0f, 0f, 1f),
+			new Vector3 ( 0f, 0f, 1f), 
+			// Right face
+			new Vector3 ( 1f, 0f, 0f), 
+			new Vector3 ( 1f, 0f, 0f), 
+			new Vector3 ( 1f, 0f, 0f), 
+			new Vector3 ( 1f, 0f, 0f),
+			// Back face
+			new Vector3 ( 0f, 0f, -1f), 
+			new Vector3 ( 0f, 0f, -1f), 
+			new Vector3 ( 0f, 0f, -1f),  
+			new Vector3 ( 0f, 0f, -1f), 
+			// Left face
+			new Vector3 ( -1f, 0f, 0f),  
+			new Vector3 ( -1f, 0f, 0f), 
+			new Vector3 ( -1f, 0f, 0f),  
+			new Vector3 ( -1f, 0f, 0f),
+			// Top Face	
+			new Vector3 ( 0f, 1f, 0f),  
+			new Vector3 ( 0f, 1f, 0f), 
+			new Vector3 ( 0f, 1f, 0f),  
+			new Vector3 ( 0f, 1f, 0f),
+			// Bottom Face
+			new Vector3 ( 0f, -1f, 0f),  
+			new Vector3 ( 0f, -1f, 0f), 
+			new Vector3 ( 0f, -1f, 0f),  
+			new Vector3 ( 0f, -1f, 0f)
+		};
+
+		int[] indexData = new int[] {
+			// Font face
+			0, 1, 2, 2, 3, 0, 
+			// Right face
+			7, 6, 5, 5, 4, 7, 
+			// Back face
+			11, 10, 9, 9, 8, 11, 
+			// Left face
+			15, 14, 13, 13, 12, 15, 
+			// Top Face	
+			19, 18, 17, 17, 16, 19,
+			// Bottom Face
+			23, 22, 21, 21, 20, 23
+		};
 
 		int shaderProgramHandle, vertexShaderHandle, fragmentShaderHandle;
-		int posVbo, indexVbo;
+		int posVbo, /*normVbo,*/ indexVbo;
 		int vaoId;
 
 		int modelviewMatrixLoc, projectionMatrixLoc;
@@ -87,6 +143,10 @@ void main()
 			GL.BindBuffer (BufferTarget.ArrayBuffer, posVbo);
 			GL.BufferData<Vector3> (BufferTarget.ArrayBuffer, new IntPtr (positionData.Length * Vector3.SizeInBytes), positionData, BufferUsageHint.StaticDraw);
 
+//			GL.GenBuffers (1, out normVbo);
+//			GL.BindBuffer (BufferTarget.ArrayBuffer, posVbo);
+//			GL.BufferData<Vector3> (BufferTarget.ArrayBuffer, new IntPtr (normalData.Length * Vector3.SizeInBytes), normalData, BufferUsageHint.StaticDraw);
+
 			GL.GenBuffers (1, out indexVbo);
 			GL.BindBuffer (BufferTarget.ElementArrayBuffer, indexVbo);
 			GL.BufferData (BufferTarget.ElementArrayBuffer, new IntPtr (indexData.Length * sizeof(int)), indexData, BufferUsageHint.StaticDraw);
@@ -102,6 +162,11 @@ void main()
 			GL.BindBuffer (BufferTarget.ArrayBuffer, posVbo);
 			GL.VertexAttribPointer (0, 3, VertexAttribPointerType.Float, true, Vector3.SizeInBytes, 0);
 			GL.BindAttribLocation (shaderProgramHandle, 0, "in_position");
+
+//			GL.EnableVertexAttribArray (1);
+//			GL.BindBuffer (BufferTarget.ArrayBuffer, normVbo);
+//			GL.VertexAttribPointer (1, 3, VertexAttribPointerType.Float, true, Vector3.SizeInBytes, 0);
+//			GL.BindAttribLocation (shaderProgramHandle, 1, "in_normal");
 
 			GL.BindBuffer (BufferTarget.ElementArrayBuffer, indexVbo);
 			GL.BindVertexArray (0);
